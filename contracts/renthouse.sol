4c833fad8a93bc32b69ca7b47b8cbd9e72353819
丁文軍 <eric_dwj@icloud.com>
2018-03-10 12:38:33 +0900

    add rent house and token smart contract.

---
+++ contracts/renthouse.sol
@@ -0,0 +1,94 @@
+pragma solidity ^0.4.4;
+contract RentHouse{
+    
+    struct RentContract{
+        string doorkey;
+        uint price;
+        uint start;
+        uint end;
+        uint contractdatetime;
+        uint state;//0 close , 1 open
+        address owner;
+       
+    }
+    
+    mapping ( uint  => RentContract ) rentContracts;
+    
+    uint[] public uuids;
+   
+    function setRentContracts(uint _uuid, string _doorkey,uint _price,uint _start,uint _end) returns  (bool success)
+    {
+        rentContracts[_uuid]
+        = RentContract(
+                                 {
+                                    doorkey : _doorkey,
+                                    price   : _price,
+                                    start   : _start,
+                                    end     : _end,
+                                    contractdatetime:block.timestamp,
+                                    owner   : msg.sender,
+                                    state   : 1
+                                }
+
+              );
+        
+        uuids.push(_uuid);
+         
+        return true;
+    }
+    
+    function getUUIDS() view public returns(uint[]) {
+        return uuids;
+    }
+    
+    function getRentContract(uint _uuid)  view  returns (uint _price,uint _start, uint _end, uint _contractdatetime,address _owner,uint _state)  
+    {
+        
+        //check the contract list, the most important thing is that if state is 0, that means this house had been rented.       
+        return (
+            rentContracts[_uuid].price, 
+            rentContracts[_uuid].start,
+            rentContracts[_uuid].end,
+            rentContracts[_uuid].contractdatetime,
+            rentContracts[_uuid].owner,
+            rentContracts[_uuid].state
+            );
+    }
+      
+    event RentResult(string _doorkey, uint _price, uint _start, uint _end, uint _contractdatetime);
+    
+    function Rent(uint _uuid,address token) returns (bool success)  {
+        
+         //remember to call Token.approve(this_contract_address,mount)
+         //tranfer token to houseowner
+         if(rentContracts[_uuid].state == 0)
+         {
+             //the contract had been closed
+             return false;
+         }
+         
+         
+         if(Token(token).transferFrom(msg.sender,rentContracts[_uuid].owner,rentContracts[_uuid].price))
+         {
+             //set the contract to close
+             rentContracts[_uuid].state = 0;
+             RentResult (
+                            rentContracts[_uuid].doorkey, 
+                            rentContracts[_uuid].price, 
+                            rentContracts[_uuid].start,
+                            rentContracts[_uuid].end,
+                            rentContracts[_uuid].contractdatetime
+                            );
+                            
+            return true;
+             
+         }
+         else
+         {
+             //transfer token failure
+             return false;
+         }
+         
+    }
+
+}
