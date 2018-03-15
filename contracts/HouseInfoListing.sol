18b820bb7af42bdbfd3295f5b688c1b5f3e6ef89
丁文軍 <eric_dwj@icloud.com>
2018-03-15 21:15:15 +0900

    add preOrder logic to the smart contract

--- contracts/HouseInfoListing.sol
+++ contracts/HouseInfoListing.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+
 contract HouseInfoListing{
     
   struct HouseInfo {
@@ -12,10 +13,36 @@ contract HouseInfoListing{
     bytes32 ipfsHash;
   }
     
-  mapping ( uint => HouseInfo ) private houseInfo;
-  mapping ( address => uint[] ) private uuids;
+  mapping ( bytes32 => HouseInfo ) private houseInfo;//describ the house information
+  mapping ( address => bytes32[] ) private uuids;//every house info has one uuid,find house info by house owner address
+  //should add find house info by city street 
+  mapping ( bytes32 => address[] ) private PreOrders;//find preorders lists by house info uuid 
+                                                     // find preOrder or order infomation from this connection 
   
-   function setHouseInfo(uint _uuid, string _doorkey,string _houseaddress,uint _price,bytes32 _ipfsHash) 
+  function preOrder()
+  public
+  returns (bool success)
+  {
+      //1:new preOrder contract ,add house info , guest address, date from , date to etc info to preorder contract
+      //2:transfer token to contract address
+      //3:if step 2 successfully finished, add preOrder contract address to PreOrders lists
+      
+      return true;
+      
+  }
+  
+  function cancelOrder()
+  public
+  returns(bool success)
+  {
+      //1 transfer token from contract address to guest address
+      //2 if step 1 successfully finished, update info of preOrder contract
+      //3 if step 1 successfully finished, remove order info from PreOrders lists
+       return true;
+  }
+  
+  
+   function setHouseInfo(bytes32 _uuid, string _doorkey,string _houseaddress,uint _price,bytes32 _ipfsHash) 
    public 
    returns(bool success)
   {
@@ -38,12 +65,12 @@ contract HouseInfoListing{
   function getUUIDS(address houseowner)
     view
     public
-    returns(uint[] _uuid)
+    returns(bytes32[] _uuid)
   {
     return uuids[houseowner];
   }
     
-  function getHouseInfo(uint _uuid)
+  function getHouseInfo(bytes32 _uuid)
     view
     public
     returns (string _houseaddress,uint _price, uint _contractdatetime, address _owner,uint _state,bytes32 _ipfsHash)
