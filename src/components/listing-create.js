cebb5d3f1e6f426534f59e2758fb6c94157e0f3c
丁文軍 <eric_dwj@icloud.com>
2018-04-02 12:28:36 +0900

    update create host listing

--- src/components/listing-create.js
+++ src/components/listing-create.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import houselistingService from '../services/houseinfolist-service';
 import ListingDetail from './listing-detail';
 import Overlay from './overlay';
+import hostService from '../services/host-service';
 
 
 
@@ -11,64 +12,91 @@ const alertify = require('../../node_modules/alertify/src/alertify.js');
 
 class ListingCreate extends Component {
 
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.STEP = {
-      DETAILS: 1,
-      PREVIEW: 2,
-      METAMASK: 3,
-      PROCESSING: 4,
-      SUCCESS: 5
+        this.STEP = {
+            STEP1: 1,
+            STEP2: 2,
+            METAMASK: 3,
+            PROCESSING: 4,
+            SUCCESS: 5
+        }
+
+        this.state = {
+            step: 0,
+            category:"",
+            beds:0,
+            location:"",
+            user: {}
+        }
+
+        this.finishStep1 = this.finishStep1.bind(this)
     }
 
-    this.state = {
-      step     : this.STEP.DETAILS,
-      category : "",
-      beds     : 0 , 
-      location : "",
-      price    : 0 , 
-      pictures : []
+    finishStep1() {
+      this.setState({step:this.STEP.STEP2});
+
     }
 
-    this.onDetailsEntered = this.onDetailsEntered.bind(this)
-  }
+    componentWillMount() {
+        
+        this.setState({step:this.STEP.STEP1});
+        window.web3.eth.getAccounts((error, accounts) => {
+            this.setState({
+                account: accounts[0],
+                id: accounts[0]
+            });
 
-  onDetailsEntered(formListing) {
+            hostService.getHostInfo(accounts[0]).then((data) => {
+                this.setState({
+                    user: data
+                });
+            });
 
-  }
+        });
+    }
 
-  onSubmitListing(formListing) {
+    onSubmitListing(formListing) {
 
-    this.setState({ step: this.STEP.METAMASK });
-    
-    houselistingService.submitListing(formListing)
-    .then((tx) => {
-      this.setState({ step: this.STEP.PROCESSING });
-      return houselistingService.waitTransactionFinished(tx);
-    })
-    .then((blockNumber) => {
-      this.setState({ step: this.STEP.SUCCESS });
-    })
-    .catch((error) => {
-      alertify.log(error.message);
-    })
-  }
+        this.setState({
+            step: this.STEP.METAMASK
+        });
+
+        houselistingService.submitListing(formListing)
+            .then((tx) => {
+                this.setState({
+                    step: this.STEP.PROCESSING
+                });
+                return houselistingService.waitTransactionFinished(tx);
+            })
+            .then((blockNumber) => {
+                this.setState({
+                    step: this.STEP.SUCCESS
+                });
+            })
+            .catch((error) => {
+                alertify.log(error.message);
+            })
+    }
 
   render() {
     return (
       <div className="becomehost-1 container">
         <br/><br/>
-        { this.state.step === this.STEP.DETAILS &&
+        { this.state.step === this.STEP.STEP1 &&
 
             <div className="row">
               <div className="col-md-6 col-lg-6 col-sm-6">
-                  <label>STEP {Number(this.state.step)}</label>
-                  <h2>Create your listing</h2>
+              <img className="becomehost__step-1" src="../images/becomehost-step.png" alt=""/>
+                  <h1>Hi,{this.state.user.user}!,Let's get started listing your space</h1>
 
-                  <div className="form-group">
-                      <label>Type*</label>
-                      <select className="form-control">
+                  <h2>What's kind of place do you have?</h2>
+
+                  <div className="row">
+                  <div className="col-md-6 form-group">
+                      <label>Category*</label>
+                      <select className="form-control" onChange={(e) => this.setState({category: e.target.value})}>
                         <option>Entire Place</option>
                         <option>Private Room</option>
                         <option>Share Room</option>
@@ -76,9 +104,9 @@ class ListingCreate extends Component {
                   </div>
 
 
-                  <div className="form-group">
+                  <div className="col-md-6 form-group">
                       <label>Beds*</label>
-                      <select className="form-control">
+                      <select className="form-control" onChange={(e) => this.setState({beds: e.target.value})}>
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -86,16 +114,18 @@ class ListingCreate extends Component {
                         <option>5</option>
                       </select>
                   </div>
+                  </div>
+
 
                   <div className="form-group">
                     <label>Location*</label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" className="form-control" onChange={(e) => this.setState({location: e.target.value})} />
                   </div>
 
-                  <div className="form-group">
-                    <label>Price in PPS*</label>
-                    <input type="number" className="form-control"/>
-                  </div>
+                  <a className="btn btn-default btn-lg bg-pink color-white" onClick={this.finishStep1} href="">Continue</a>
+
+                  <img src="../images/becomehost-step1-hint.jpg" alt=""/>
+
 
 
               </div>
@@ -107,6 +137,12 @@ class ListingCreate extends Component {
             
             
           </div>
+        }
+
+        {
+          this.state.step === this.STEP.STEP2 &&
+          <h2>Hello world</h2>
+
         }
       </div>
     )
