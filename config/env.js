b00908619a10eaf12fa2aacbe536ac7d2dadf768
丁文軍 <eric_dwj@icloud.com>
2018-04-07 10:52:51 +0900

    new contract

--- config/env.js
+++ config/env.js
@@ -13,7 +13,7 @@ function getClientEnvironment(publicUrl) {
       'PUBLIC_URL': JSON.stringify(publicUrl),
       'IPFS_API_PORT': JSON.stringify("5001"),
       'IPFS_DOMAIN': JSON.stringify("ipfs.infura.io"),
-      'RentHouseListingAddress':JSON.stringify("0x0b0083027863239fb72194a640bb0a9d476dc722"),
+      'RentHouseListingAddress':JSON.stringify("0xd5e24bc3697a74e6c7caa88c5d855e0c1646518b"),
       'PPSAddress':JSON.stringify("0x7184589d0e38ebdcee9a8e046845d665bf33c782"),
       'Server_Address':JSON.stringify("http://133.130.99.204:1338/")
 
