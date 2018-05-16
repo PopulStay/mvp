0c0100d3df65f13740d96ab266af13df610259df
王艳 <13753068898@163.com>
2018-05-16 11:27:19 +0800

    VerifyID

--- src/components/verify-id.js
+++ src/components/verify-id.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 import GuestRegister from './guest-register';
+import houselistingService from '../services/houseinfolist-service';
 
 class VerifyID extends Component {
 
@@ -13,10 +14,8 @@ class VerifyID extends Component {
             Step3:3,
             Step4:4,
             Step5:5,
-            Step6:6,
-            Step7:7,
-            Step8:8,
-            Step9:9,
+            PROCESSING: 6,
+            SUCCESS: 7,
     }
 
     this.state = {
@@ -25,7 +24,31 @@ class VerifyID extends Component {
         Countrysarr:["Angola","Afghanistan","Albania","Algeria","Anguilla","Antigua and Barbuda","Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda. ","Bolivia","Botswana","Brunei "," Bulgaria","Bulgaria","Burkina"," Burma"," Burundi ","Canada","the Central African Republic","Chad","Bolivia","Columbia","Congo","the Cook islands","Costa Rica","Cuba","Czech","Denmark","Denmark","Djibouti","Djibouti","Ecuador","Salvatore","Estonia ","Ethiopia","Fiji","Finland","French","French Guiana","Gabon"," Georgia "," German "," Garner "," Gibraltar "," Greece","Grenada","Guam "," Guatemala"," Guinea "," Guyana "," Haiti,"," Honduras,","Honduras","Hongkong","Hungary","Iceland","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kazakhstan","Kenya","South Korea","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Italy","Liechtenstein","Lithuania","Macao","Madagascar","Mawlawi","Malaysia","Maldives","Mali","Malta","Mauritius","Mexico","Moldova","Monaco","Mongolia","Mont salad","Morocco","Mozambique","Malta","Neo","Nepal","New Zealand","New Zealand","Nicaragua "," Niger"," Nigeria "," Norway ","Oman","Pakistan "," Papua New Guinea","Paraguay","Peru","Philippines","Poland","French Polynesia","Portuguese"," Puerto Rico "," Qatar "," Russia "," Saint Lucia ","St. Lucia","Saint Mari"," St. Mari "," Sao Tome and Principe "," Sao Tome and Principe "," Senegal","Seychelles"," Sierra Leone"," Singapore ","Slovakia"," Slovenia "," Somalia","South Africa","Senegal","Sri Lanka","Sultan"," Swaziland "," Sweden "," Switzerland"," the Swiss "," the Taiwan Province","the Taiwan Province","Tajikistan","the Tajikistan","Tanzania","Thailand","Togo","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Venezuela","Vietnam","Yemen","Turkey"],
         Countrys:"Singapore",
         Certificates_type:1,
+        Photo_front:"",
+        Photo_back:"",
     };
+  }
+
+  uploadfront(e,index){
+      var files_front = this.state.Photo_front;
+      var files_back = this.state.Photo_back;
+      let reader = new FileReader();
+      let file = event.target.files[0];
+      reader.onloadend = () => {
+        if(index == 1){
+            this.setState({Photo_front:reader.result});
+        }else{
+            this.setState({Photo_back:reader.result});
+        }
+      }
+      reader.readAsDataURL(file)
+  }
+
+  nextstep(e){
+    this.setState({step:this.state.step+1});
+  }
+  submit(e){
+    
   }
 
   render() {
@@ -34,12 +57,12 @@ class VerifyID extends Component {
     return (
       <div className="VerifyID">
           <div><img className="header__logo" src="../images/logo.png" alt=""/></div>
-          { this.state.step === this.STEP.Step10 &&
+          { this.state.step === this.STEP.Step1 &&
               <div className="VerifyID_1 VerifyID_content col-lg-offset-1 col-lg-7">
                   <img src="/images/rightBoximg.png" />
                   <h3>Add a valid government ID</h3>
                   <h5>It looks like the photo you added isn’t a valid form of ID. To help us make sure it’s you, we’ll need another photo of a valid ID, like a driver’s license, passport, or visa.</h5>
-                  <button className="next">Add another photo</button>
+                  <button className="next" onClick={(e)=>this.nextstep(e)}>Add another photo</button>
               </div>
           }
 
@@ -59,7 +82,7 @@ class VerifyID extends Component {
                   </div>
 
                   <h6><span className="glyphicon glyphicon-lock"></span>Your ID will never be shared with a guest.</h6>
-                  <button className="next">Add another photo</button>
+                  <button className="next"  onClick={(e)=>this.nextstep(e)}>Add another photo</button>
               </div>
           }
 
@@ -90,30 +113,41 @@ class VerifyID extends Component {
                   </div>
 
                   <h6><span className="glyphicon glyphicon-lock"></span>Your ID will never be shared with a guest.</h6>
-                  <button className="next">Next</button>
+                  <button className="next"  onClick={(e)=>this.nextstep(e)}>Next</button>
               </div>
           }
 
-          { this.state.step === this.STEP.Step1 &&
-              <div className="VerifyID_2 VerifyID_3 VerifyID_content col-lg-offset-1 col-lg-7">
+          { this.state.step === this.STEP.Step4 &&
+              <div className="VerifyID_2 VerifyID_4 VerifyID_content col-lg-offset-1 col-lg-7">
                   <h3>Does this look OK?</h3>
                   <h5>Make sure your images aren’t blurry and the front clearly shows your</h5>
-                  <p className="text1">Issuing country</p>
-
-                  <div className="Left col-lg-6">
-                    <div className="photodiv">
-                      <input type="file" />
-                      <p>Add front</p>
-                    </div>
-                  </div> 
-                  <div className="Right col-lg-6">
-                    <div className="photodiv">
-                      <input type="file" />
-                      <p>Add back</p>
-                    </div>
+                  <div className="divbox">
+                    <div className="Left col-lg-6">
+                      <div className="photodiv">
+                        <img className={this.state.Photo_front ? "show" : "hide"} src={this.state.Photo_front} />
+                        <input type="file" onChange={(e)=>this.uploadfront(e,1)} />
+                      </div>
+                        <p>Add front</p>
+                    </div> 
+                    <div className="Right col-lg-6">
+                      <div className="photodiv">
+                        <img className={this.state.Photo_back ? "show" : "hide"} src={this.state.Photo_back} />
+                        <input type="file"  onChange={(e)=>this.uploadfront(e,2)}/>
+                      </div>
+                        <p>Add back</p>
+                    </div> 
                   </div> 
                   
-                  <button className="next">Next</button>
+                  <button className="next"  onClick={(e)=>this.nextstep(e)}>Next</button>
+              </div>
+          }
+
+          { this.state.step === this.STEP.Step5 &&
+              <div className="VerifyID_1 VerifyID_content col-lg-offset-1 col-lg-7">
+                  <h3>We’re still reviewing your ID</h3>
+                  <h5>This will get wrapped up as soon as possible—and we’ll be in touch if there’s anything else we need.</h5>
+                  <h5>In the meantime, you can pick up where you left off.</h5>
+                  <button className="next"  onClick={(e)=>this.submit(e)}>Continue</button>
               </div>
           }
       </div>
