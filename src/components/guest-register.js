3e4067ec984b08497866a9d43f67edf3dede5170
王艳 <13753068898@163.com>
2018-05-18 22:43:18 +0800

    navbar

--- src/components/guest-register.js
+++ src/components/guest-register.js
@@ -6,7 +6,6 @@ import guestService from '../services/guest-service';
 import Wallet from './wallet';
 import Web3 from 'web3';
 import web3service from '../services/web3-service';
-import WalletClear from './walletClear';
 
 
 const customStyles = {
@@ -96,9 +95,6 @@ class GuestRegister extends React.Component {
 
   }
 
-  onLogOut = () =>{
-    this.setState({ registered:false });
-  }
 
 
   render() {
@@ -111,7 +107,6 @@ class GuestRegister extends React.Component {
            <Link to="/managepanel">
           <button className="logoutButton float-right">Welcome！{this.state.user}<span></span></button>
           </Link>
-          <WalletClear onLogOut={this.onLogOut} />
           </div>
 
         }
