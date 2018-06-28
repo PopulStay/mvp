475b43a6f12dd9ef8bcb86addab90a62de5a74e6
王艳 <13753068898@163.com>
2018-06-28 10:54:16 +0800

    Email

--- public/populstay_webpage/js/main.js
+++ public/populstay_webpage/js/main.js
@@ -4,14 +4,18 @@ window.onload = function () {
   $("#submit").click(function(){
   	var Email = $("#Email").val()
   	var subject = $("#Address").val()
-  	var text = $("#Message").val()
+    var text = $("#Message").val()
+    var name = $("#Name").val()
+  	var telephone = $("#Phone").val()
   	
 	$.post("https://server.populstay.com/emailsender",
 	{
 	  from:'walter@populstay.com',
 	  to:Email,
 	  subject:subject,
-	  text:text
+	  text:text,
+    telephone:telephone,
+    name:name
 	});
 
   })
