5baf821fe765ef67079f5a335b1177179b64dfc7
王艳 <13753068898@163.com>
2018-06-30 11:31:44 +0800

    语言国际化

--- public/populstay_webpage/js/Language.js
+++ public/populstay_webpage/js/Language.js
@@ -1,101 +1,91 @@
-
-  	var url='http://pv.sohu.com/cityjson';
-   $.ajax({  
-        url:"http://pv.sohu.com/cityjson",  
-        dataType:'jsonp',  
-        data:'',  
-        jsonp:'callback',  
-        success:function(result) {  
-            console.log(result)
-        },  
-    });  
-
-
-	// console.log(returnCitySN["cip"])
-		// $(".header__title").html("PopulStay");
-		// $(".header__description1").html("The genuine home sharing community redesigned<br>using blockchain technology");
-		// $(".header__description2").html("The decentralized booking and autonomous property management platform<br/> for vacation rental and the home sharing economy.<br/> PopulStay has been selected as part of the Blockchain Business Camp Tokyo.<br/> Details can be found");
-		// $(".header__demo-button-text").html("Visit PopulStay Demo");
-		// $(".advantage__item1").html("Our Benefits");
-		// $(".advantage__item2").html("Blockchain-based<br/>Smart Lock");
-		// $(".advantage__item3").html("Decentralized Platform");
-		// $(".advantage__item6").html("No middleman");
-		// $(".advantage__item4").html("Machine Trust");
-		// $(".advantage__item7").html("Defined by smart contracts");
-		// $(".advantage__item5").html("Decentralized Autonomous<br/>Organization");
-		// $(".advantage__item8").html("Operate more efficiently");
-		// $(".product__subtitle").html("Hardware: The world’s first real keyless door look.");
-		// $(".story-product__description1").html("The biggest challenge and hidden danger of smart locks is security.");
-		// $(".story-product__description2").html("For the hardware part, the cylinder in the lock allows the locksmith or thieve to open the door lock easily, it is just a matter of time, depending on the grade of the cylinder. For the software part, a central door lock management system means that once the server is hacked, hackers will acquire the control rights of all the door locks at zero cost. This would be a catastrophic event.");
-		// $(".story-product__description3").html("PopulStay, however, developed the world's first genuine, keyless, blockchain-based smart lock and its associated chips.The door locks are managed through a smart contract. The owner of the door lock can determine, who can use the door lock and access the property during which period, and the user of the door lock needs to confirm the transaction of the door lock with a smart phone and gets authenticated first.");
-		// $(".story-product__description4").html("In our scenario, the door lock will deduct the rental fee from the frozen PPS token and return the balance to the user after usage. The rental fee will be transferred to the host automatically. The process completely eliminates the need of a third party.Those smart door locks and smart key boxes for e.g. houses, cars, washing machines, bikes and anything else that can be locked, allow to bring extra income to individuals or businesses.");
-		// $(".story-product__description5").html("We are convinced that smart locks will revolutionize the sharing economy and even become the infrastructure for the future economy.");
-		// $(".lock-design__title1").html("Non Blockchain-based Door Lock Design");
-		// $(".lock-design__title2").html("Blockchain-based Door Lock Design");
-		// $(".platform__intro-text").html("Software: The home sharing economy redesigned using blockchain.")
-		// $(".platform__subtitle").html("No middleman");
-		// $(".platform__description").html("Popular vacation rental or home sharing platforms established trust by being the dependable middleman to connect the host and guest. Using the blockchain technology, the decentralized platform is able to connect host and guest with transparency, fairness and trust without the need of middlemen and the hefty transaction fees they charge.");
-		// $(".organization__subtitle").html("Operate more efficiently");
-		// $(".organization__description").html("Publishing a listing on our platform takes less than ten minutes, but operating a listing takes efforts. In order to make it hassle-free, we integrate qualified third party providers for cleaning, key exchange, professional photo shooting, interior design and dynamic pricing to work for you in a decentralized and autonomous way.");
-		// $(".machine__subtitle").html("Defined by smart contract");
-		// $(".machine__description").html("Host and guest payment, guest dispute, additional service fee, property management fee – all these payments are defined my smart contracts. No manual override, no cash payment for booking. More trust and more efficiency.");
-		// $(".communication__subtitle").html("Direct talk and video chat");
-		// $(".communication__description").html("Communication through our platform becomes as easy as it could be by connecting host and guest through call or video chat within seconds. This way allows the parties to clarify questions or take a live view of the property. By connecting to each other the platforms tokens are used and automatically deducted.");
-		// $(".blog__title").html("Blog");
-		// $(".blog__subtitle").html("Articles & Publication");
-		// $(".blog__tag").html("Story");
-		// $(".blog__article-title").html("Early Bitcoin Investor Jeffrey Wernick meets PopulStay");
-		// $(".blog__article").html("Jeffrey Wernick, Ph.D. in Economics and Finance at the University of Chicago, and Quantum Chain Consultant, is said to be the second person in the world to hold Bitcoin and... <a class='blog__view-more-btn'>view full story</a> ");
-		// $(".blog__feedback-title").html("PopulStay.");
-		// $(".blog__feedback-title2").html("2nd of June");
-		// $(".blog__social-container label").html("Visit Us:");
-		// $(".services__title").html("Services");
-		// $(".services__subtitle").html("Third Party Services");
-		// $(".services__role-name1").html("Host");
-		// $(".services__role-name2").html("Guest");
-		// $(".services__role-effect-text1").html("Cleaning & Washing");
-		// $(".services__role-effect-text2").html("Photo Shooting");
-		// $(".services__role-effect-text3").html("Smart ioT Gadgets");
-		// $(".services__role-effect-text4").html("Smart Pricing");
-		// $(".services__role-effect-text5").html("Marketing & Branding");
-		// $(".services__role-effect-text6").html("Interior Design");
-		// $(".services__role-effect-text7").html("Transportation");
-		// $(".services__role-effect-text8").html("Emergency Support");
-		// $(".services__role-effect-text9").html("AI Chatbot");
-		// $(".services__role-effect-text10").html("Local Guide");
-		// $(".services__role-effect-text11").html("Restaurants");
-		// $(".services__role-effect-text12").html("Medical Tourist");
-		// $(".intro__team-item-name-wang").html("Walter Wang Yue");
-		// $(".intro__team-item-name-cai").html("Xinpeng Cai");
-		// $(".intro__team-item-name-lin").html("Brian Lin");
-		// $(".intro__team-item-name-zhong").html("Christie Zhong");
-		// $(".intro__team-item-name-e").html("Jiachang E");
-		// $(".intro__team-item-name-jun").html("Dingwen Jun");
-		// $(".item-position1").html("Founder & CEO");
-		// $(".item-position2").html("Co-founder & COO");
-		// $(".item-position3").html("Chief Marketing Officer");
-		// $(".item-position4").html("Senior Designer");
-		// $(".item-position5").html("Marketing Guru");
-		// $(".item-position6").html("Technology Guru");
-		// $(".subscribe__title").html("Subscribe to our Newsletter");
-		// $("#subscribeSubmit").html("SIGN UP");
-		// $(".contact-us__textarea label").html("Message");
-		// $(".liname span").html("Walter Wang Yue");
-		// $(".Download").html("Download");
-		// $(".Download strong").html("White Paper :");
-		// $(".contact-us__tag1 i").html("PDF");
-		// $(".contact-us__tag2 i").html("Google Doc");
-		// $(".footer__contact-title").html("Contact Us");
-		// $(".footer__contact-address").html("10 Anson Road #27-15 lntenational Plaza, Singapore 079903");
-		// $(".footer__follow-title").html("Follow Us");
-		// $("#prompt h3").html("Submitted successfully");
-		// $("#prompt button").html("CLOSE");
-		// $("#contactName").attr('placeholder','Name')
-		// $("#contactPhone").attr('placeholder','Phone')
-		// $("#contactEmail").attr('placeholder','Email')
-		// $("#contactAddress").attr('placeholder','Address')
-		// $("#subscribeEmail").attr('placeholder','Enter your email address')
-
+$("header h6").click(function(){
+	if($("header h6").html() == "English"){
+		$("header h6").html("中文");
+		$(".header__title").html("PopulStay");
+		$(".header__description1").html("The genuine home sharing community redesigned<br>using blockchain technology");
+		$(".header__description2").html("The decentralized booking and autonomous property management platform<br/> for vacation rental and the home sharing economy.<br/> PopulStay has been selected as part of the Blockchain Business Camp Tokyo.<br/> Details can be found");
+		$(".header__demo-button-text").html("Visit PopulStay Demo");
+		$(".advantage__item1").html("Our Benefits");
+		$(".advantage__item2").html("Blockchain-based<br/>Smart Lock");
+		$(".advantage__item3").html("Decentralized Platform");
+		$(".advantage__item6").html("No middleman");
+		$(".advantage__item4").html("Machine Trust");
+		$(".advantage__item7").html("Defined by smart contracts");
+		$(".advantage__item5").html("Decentralized Autonomous<br/>Organization");
+		$(".advantage__item8").html("Operate more efficiently");
+		$(".product__subtitle").html("Hardware: The world’s first real keyless door look.");
+		$(".story-product__description1").html("The biggest challenge and hidden danger of smart locks is security.");
+		$(".story-product__description2").html("For the hardware part, the cylinder in the lock allows the locksmith or thieve to open the door lock easily, it is just a matter of time, depending on the grade of the cylinder. For the software part, a central door lock management system means that once the server is hacked, hackers will acquire the control rights of all the door locks at zero cost. This would be a catastrophic event.");
+		$(".story-product__description3").html("PopulStay, however, developed the world's first genuine, keyless, blockchain-based smart lock and its associated chips.The door locks are managed through a smart contract. The owner of the door lock can determine, who can use the door lock and access the property during which period, and the user of the door lock needs to confirm the transaction of the door lock with a smart phone and gets authenticated first.");
+		$(".story-product__description4").html("In our scenario, the door lock will deduct the rental fee from the frozen PPS token and return the balance to the user after usage. The rental fee will be transferred to the host automatically. The process completely eliminates the need of a third party.Those smart door locks and smart key boxes for e.g. houses, cars, washing machines, bikes and anything else that can be locked, allow to bring extra income to individuals or businesses.");
+		$(".story-product__description5").html("We are convinced that smart locks will revolutionize the sharing economy and even become the infrastructure for the future economy.");
+		$(".lock-design__title1").html("Non Blockchain-based Door Lock Design");
+		$(".lock-design__title2").html("Blockchain-based Door Lock Design");
+		$(".platform__intro-text").html("Software: The home sharing economy redesigned using blockchain.")
+		$(".platform__subtitle").html("No middleman");
+		$(".platform__description").html("Popular vacation rental or home sharing platforms established trust by being the dependable middleman to connect the host and guest. Using the blockchain technology, the decentralized platform is able to connect host and guest with transparency, fairness and trust without the need of middlemen and the hefty transaction fees they charge.");
+		$(".organization__subtitle").html("Operate more efficiently");
+		$(".organization__description").html("Publishing a listing on our platform takes less than ten minutes, but operating a listing takes efforts. In order to make it hassle-free, we integrate qualified third party providers for cleaning, key exchange, professional photo shooting, interior design and dynamic pricing to work for you in a decentralized and autonomous way.");
+		$(".machine__subtitle").html("Defined by smart contract");
+		$(".machine__description").html("Host and guest payment, guest dispute, additional service fee, property management fee – all these payments are defined my smart contracts. No manual override, no cash payment for booking. More trust and more efficiency.");
+		$(".communication__subtitle").html("Direct talk and video chat");
+		$(".communication__description").html("Communication through our platform becomes as easy as it could be by connecting host and guest through call or video chat within seconds. This way allows the parties to clarify questions or take a live view of the property. By connecting to each other the platforms tokens are used and automatically deducted.");
+		$(".blog__title").html("Blog");
+		$(".blog__subtitle").html("Articles & Publication");
+		$(".blog__tag").html("Story");
+		$(".blog__article-title").html("Early Bitcoin Investor Jeffrey Wernick meets PopulStay");
+		$(".blog__article").html("Jeffrey Wernick, Ph.D. in Economics and Finance at the University of Chicago, and Quantum Chain Consultant, is said to be the second person in the world to hold Bitcoin and... <a class='blog__view-more-btn'>view full story</a> ");
+		$(".blog__feedback-title").html("PopulStay.");
+		$(".blog__feedback-title2").html("2nd of June");
+		$(".blog__social-container label").html("Visit Us:");
+		$(".services__title").html("Services");
+		$(".services__subtitle").html("Third Party Services");
+		$(".services__role-name1").html("Host");
+		$(".services__role-name2").html("Guest");
+		$(".services__role-effect-text1").html("Cleaning & Washing");
+		$(".services__role-effect-text2").html("Photo Shooting");
+		$(".services__role-effect-text3").html("Smart ioT Gadgets");
+		$(".services__role-effect-text4").html("Smart Pricing");
+		$(".services__role-effect-text5").html("Marketing & Branding");
+		$(".services__role-effect-text6").html("Interior Design");
+		$(".services__role-effect-text7").html("Transportation");
+		$(".services__role-effect-text8").html("Emergency Support");
+		$(".services__role-effect-text9").html("AI Chatbot");
+		$(".services__role-effect-text10").html("Local Guide");
+		$(".services__role-effect-text11").html("Restaurants");
+		$(".services__role-effect-text12").html("Medical Tourist");
+		$(".intro__team-item-name-wang").html("Walter Wang Yue");
+		$(".intro__team-item-name-cai").html("Xinpeng Cai");
+		$(".intro__team-item-name-lin").html("Brian Lin");
+		$(".intro__team-item-name-zhong").html("Christie Zhong");
+		$(".intro__team-item-name-e").html("Jiachang E");
+		$(".intro__team-item-name-jun").html("Dingwen Jun");
+		$(".item-position1").html("Founder & CEO");
+		$(".item-position2").html("Co-founder & COO");
+		$(".item-position3").html("Chief Marketing Officer");
+		$(".item-position4").html("Senior Designer");
+		$(".item-position5").html("Marketing Guru");
+		$(".item-position6").html("Technology Guru");
+		$(".subscribe__title").html("Subscribe to our Newsletter");
+		$("#subscribeSubmit").html("SIGN UP");
+		$(".contact-us__textarea label").html("Message");
+		$(".liname span").html("Walter Wang Yue");
+		$(".Download").html("Download");
+		$(".Download strong").html("White Paper :");
+		$(".contact-us__tag1 i").html("PDF");
+		$(".contact-us__tag2 i").html("Google Doc");
+		$(".footer__contact-title").html("Contact Us");
+		$(".footer__contact-address").html("10 Anson Road #27-15 lntenational Plaza, Singapore 079903");
+		$(".footer__follow-title").html("Follow Us");
+		$("#prompt h3").html("Submitted successfully");
+		$("#prompt button").html("CLOSE");
+		$("#contactName").attr('placeholder','Name')
+		$("#contactPhone").attr('placeholder','Phone')
+		$("#contactEmail").attr('placeholder','Email')
+		$("#contactAddress").attr('placeholder','Address')
+		$("#subscribeEmail").attr('placeholder','Enter your email address')
+	}else{
+		$("header h6").html("English");
 		$(".header__title").html("潮箱");
 		$(".header__description1").html("真正的家庭共享社区使用区块链技术进行了重新设计");
 		$(".header__description2").html("度假租赁和家庭共享经济的分散预订和自主物业管理平台。<br/>潮箱已被选为东京连锁商业营的一部分。 详情可以找到");
@@ -179,3 +169,8 @@
 		$("#contactEmail").attr('placeholder','邮箱')
 		$("#contactAddress").attr('placeholder','地址')
 		$("#subscribeEmail").attr('placeholder','输入你的电子邮箱地址')
+	}
+})
+
+
+		
